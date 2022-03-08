2fc886f960340dc7099693296a857d2cc1afa7be
thientai2712 <thienta2712@gmail.com>
2022-03-08 11:05:43 +0700

    Firstcommit

---
+++ demo.js
@@ -0,0 +1,13 @@
+//alert("Đã tạo một trang web");
+//var b = prompt("Nhập vào một giá trị A");
+///console.log("Giá trị biến b là: ", b);
+//console.log("Thông báo gì đó");
+//console.log("Giá trị là: ", 1);
+var x,y,z
+x=5
+y=6
+//x = prompt("Nhập vào x");
+//y = prompt("Nhập vào y");
+z=parseInt(x)+parseInt(y)
+//alert("Giá trị của Z là: " +z)
+console.log("Giá trị của Z là: " +z)    
