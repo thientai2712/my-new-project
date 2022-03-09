1ff9472f081753de9c8694f16fae024ba500777e
thientai2712 <thienta2712@gmail.com>
2022-03-09 08:09:11 +0700

    firstcommit

---
+++ JAVA/java1.js
@@ -0,0 +1,8 @@
+let inputWidth;
+        let inputHeight;
+        inputHeight = prompt ('Enter the heigh');
+        inputWidth = prompt ('Enter the width');
+        let width = parseInt(inputWidth);
+        let height = parseInt(inputHeight);
+        let area = width * height ;
+        document.write("This area is: "+ area)
---
+++ JAVA/java2.js
@@ -0,0 +1,9 @@
+a = parseInt( prompt('Vật lý: '));
+b = parseInt( prompt('Hóa học: '));
+c = parseInt( prompt ('Sinh học: '));
+var diemtrungbinh = (a+b+c)/3;
+var tong = (a+b+c);
+console.log('diemtrungbinh',diemtrungbinh);
+console.log('tong',tong);
+alert(a);
+console.log('diemtrungbinh', diemtrungbinh);
